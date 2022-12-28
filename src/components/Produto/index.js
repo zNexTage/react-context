373b4790a1513d4aa75d53fbdf41ba541069a141
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-28 10:54:05 -0300

    fn to add item to the cart.

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -7,23 +7,51 @@ import { CartContext } from 'commom/context/cart';
 
 
 function Produto({
-  nome,
-  foto,
+  name,
+  photo,
   id,
-  valor,
-  unidade
+  value,
+  unity
 }) {
-  const { cart, setCart } = useContext(CartContext);  
+  const { cart, setCart } = useContext(CartContext);
+
+  /**
+   * Add an item to the cart or, if the item is already in the cart, increase the 
+   * quantity of this product;
+   * @param {*} newProduct 
+   * @returns 
+   */
+  const handleAddItem = (newProduct) => {    
+    const isCartItem = cart.some(cartItem => cartItem.id === newProduct.id);
+
+    //the item is not in the cart?
+    if (!isCartItem) {
+      newProduct.quantity = 1;
+      setCart([...cart, { ...newProduct }]);
+      return;
+    }
+
+    // the item is already in the cart; we must increase the quantity.
+    setCart(
+      cart => cart.map((cartItem) => {
+        if (cartItem.id === newProduct.id) {
+          cartItem.quantity += 1;
+        }
+
+        return cartItem;
+      })
+    )
+  }
 
   return (
     <Container>
       <div>
         <img
-          src={`/assets/${foto}.png`}
-          alt={`foto de ${nome}`}
+          src={`/assets/${photo}.png`}
+          alt={`foto de ${name}`}
         />
         <p>
-          {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+          {name} - R$ {value?.toFixed(2)} <span>Kg</span>
         </p>
       </div>
       <div>
@@ -32,7 +60,7 @@ function Produto({
         >
           <RemoveIcon />
         </IconButton>
-        <IconButton>
+        <IconButton onClick={() => handleAddItem({ id, name, photo, value, unity })}>
           <AddIcon />
         </IconButton>
       </div>
