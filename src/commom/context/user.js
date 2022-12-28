9a2e2faf0281e69ab012f03d4326c83f8a890ad4
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-28 10:12:00 -0300

    added displayName fn to UserContext.

--- src/commom/context/user.js
+++ src/commom/context/user.js
@@ -3,6 +3,16 @@ import { createContext, useState } from 'react';
 const UserContext = createContext();
 
 /**
+ * Context object accepts a displayName string property. 
+ * React DevTools uses this string to determine what to display for the context.
+ * 
+ * Basically, this property define the context name in react dev tools.
+ * Ref: https://reactjs.org/docs/context.html#contextdisplayname
+ */
+UserContext.displayName = 'Usuário';
+
+
+/**
  * Global state for user. 
  * @param {*} param0 
  * @returns 
