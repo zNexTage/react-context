6b1a9c05d9c158b401826421ff72fbe23d51654c
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-28 10:54:19 -0300

    set overlap to rectangular;
    Correct the warning: Material-UI: `overlap="rectangle"` was deprecated. Use `overlap="rectangular"` instead.
    Ref: https://stackoverflow.com/questions/72159936/material-ui-warning

--- src/pages/Feira/NavBar/index.js
+++ src/pages/Feira/NavBar/index.js
@@ -10,6 +10,7 @@ export default function NavBar() {
       <Logo />
       <IconButton>
         <Badge
+          overlap="rectangular"
           color="primary"
         >
           <ShoppingCartIcon />
